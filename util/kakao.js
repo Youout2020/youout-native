51d63f46b437d089481e66f6ca2c38fb3520b195
JIHYUNHA <humm223@gmail.com>
2020-11-26 14:31:42 +0900

    deploy: Set environment variables

--- util/kakao.js
+++ util/kakao.js
@@ -1,13 +1,13 @@
-import { env } from '../env';
+import getEnvVars from '../environment';
+const { KAKAO_API_KEY } = getEnvVars();
 
 export const translate = async (word) => {
   const encodedWord = encodeURI(word);
   const url = `https://dapi.kakao.com/v2/translation/translate?query=${encodedWord}&src_lang=kr&target_lang=en`;
-  const { REACT_APP_KAKAO_API_KEY } = env;
   const result = await fetch(url, {
     headers: {
       'content-type': 'application/json',
-      Authorization: `KakaoAK ${REACT_APP_KAKAO_API_KEY}`,
+      Authorization: `KakaoAK ${KAKAO_API_KEY}`,
       Accept: '*/*',
     }
   }).then((res) => res.json());
